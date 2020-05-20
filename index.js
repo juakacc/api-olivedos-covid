d72756750ad07cb8a67fbd1be58e36557b4b6589
Joaquim <juakacc@gmail.com>
2020-05-20 14:14:31 -0300

    Start project

---
+++ index.js
@@ -0,0 +1,8 @@
+require("dotenv").config();
+const http = require("http");
+const app = require("./src/app");
+
+const port = process.env.PORT || 8888;
+const server = http.createServer(app);
+
+server.listen(port);
---
+++ src/app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+const olivedosRoutes = require("./controllers/olivedos");
+
+const app = express();
+
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(bodyParser.json());
+
+app.use("/", olivedos);
+
+module.exports = app;
