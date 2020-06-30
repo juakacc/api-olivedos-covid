5b73a0e8f67dde5c26cfb4ef2aa725a793ceff67
Joaquim <juakacc@gmail.com>
2020-06-29 22:18:30 -0300

    Update config of cors

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 require("dotenv").config();
 const http = require("http");
+const cors = require("cors");
+
 const app = require("./src/app");
+
+app.use(cors());
 
 const port = process.env.PORT || 8888;
 const server = http.createServer(app);
